83b2318e3ee2be54bee62c88394a7e014c3290e7
crisschan <can101208@gmail.com>
2025-03-25 17:31:36 +0800

    modify restful api
    modify restful api

--- __init__.py
+++ __init__.py
@@ -7,82 +7,160 @@
 #__mtime__ = '2018/01/01'
 #__instruction__=''
 
+from doctest import BLANKLINE_MARKER
+from gettext import install
 from bottle import Bottle, run,get,post,request,static_file,redirect
+from bottle import Bottle, run, get, post, request, static_file, redirect
 from beaker.middleware import SessionMiddleware
 import json
+
+import pip
 app = Bottle()
 msUsername = ''
 msEquipmentid=''
 session_opts = {
    'session.type':'file',              #以文件的方式保存session
-   'session.cookei_expires':300,       #session过期时间为300秒
+   'session.cookie_expires':300,       #session过期时间为300秒
    'session.data_dir':'/tmp/sessions_dir', #session保存目录
    'session.auto':True               #自动保存session
     }
 
+#
+# 
+# 
+# #
+
 @app.get('/')
 @app.get('/index')
 def index():
-    return 'please input your username(your english name) and password(your english name)'
+    return json.dumps({
+        'code': 0,
+        'message': 'Success',
+        'data': {
+            'instruction': 'Please input your username(your english name) and password(your english name)'
+        }
+    })
 
 @app.post('/login')
 def login():
+    try:
+        data = request.json
+        msUsername = data.get('username')
+        sPassword = data.get('password')
 
-    msUsername = request.POST.get('username')
+        if msUsername == sPassword:
+            ssSession = request.environ.get('beaker.session')
+            ssSession['user'] = msUsername
+            ssSession.save()
 
-    sPassword = request.POST.get('password')
-    if msUsername==sPassword:
-        ssSession = request.environ.get('beaker.session')
+            return json.dumps({
+                'code': 0,
+                'message': 'Success',
+                'data': {
+                    'equipment_options': {
+                        '10001': 'Knife',
+                        '10002': 'Big Sword',
+                        '10003': 'KuiHuaBaoDian'
+                    }
+                }
+            })
+    except:
+        return json.dumps({
+            'code': 9901,
+            'message': 'Username or Password Error',
+            'data': None
+        })
 
-        ssSession['user'] = msUsername
-        ssSession.save
-
-
-        return 'please select One Equipment:\n10001:Knife\n10002:Big Sword\n10003:KuiHuaBaoDian'
-    else:
-        return 'Error 9901: Username or PassWord!!'
-@app.post('/selectEq')
+@app.post('/select')
 def selectEq():
-
-    msEquipmentid = request.POST.get('equipmentid')
-    ssSession = request.environ.get('beaker.session')
-    #print 'msEquipmentid:'+msEquipmentid
-    #print 'ssSession:'+ssSession
-    if msEquipmentid is not None:
-        if str(msEquipmentid).isdigit():
-            ssSession['equipmentid']=msEquipmentid
-            ssSession.save
-            return {'equipmentid':msEquipmentid,'Message':'your pick up equipmentid:'+msEquipmentid+' please select your  enemyid:\n20001:Terran\n20002:ORC\n20003:Undead'}
-    else:
-        return {'equipmentid':'-1','Message':'Error 9902: Your kill yourself!!'}
-
+    try:
+        data = request.json
+        msEquipmentid = data.get('equipmentid')
+        ssSession = request.environ.get('beaker.session')
+        
+        if msEquipmentid is not None and str(msEquipmentid).isdigit():
+            ssSession['equipmentid'] = msEquipmentid
+            ssSession.save()
+            return json.dumps({
+                'code': 0,
+                'message': 'Success',
+                'data': {
+                    'equipmentid': msEquipmentid,
+                    'enemy_options': {
+                        '20001': 'Terran',
+                        '20002': 'ORC',
+                        '20003': 'Undead'
+                    }
+                }
+            })
+        else:
+            return json.dumps({
+                'code': 9902,
+                'message': 'Invalid Equipment Selection',
+                'data': None
+            })
+    except:
+        return json.dumps({
+            'code': 9906,
+            'message': 'Invalid JSON Input',
+            'data': None
+        })
 
 @app.post('/kill')
 def kill():
-    sEnemyid = request.POST.get('enemyid')
-    #print 'sEnemyid'+sEnemyid
-    msEquipmentid = request.POST.get('equipmentid')
-    coockies = request.get_cookie("account", secret='some-secret-key')
-    #print sEnemyid
-    #print msEquipmentid
-    if sEnemyid is None:
-        return 'Error 9904: Your kill yourself!!'
-    if msEquipmentid is None:
-        return 'Error 9905: Your fight your enemy by nothing!And you are  died!'
-    if msEquipmentid in ['10001','10002','10003']:
-        if sEnemyid in ['20001','20002','20003']:
-
-            if (int(msEquipmentid)-int(sEnemyid)+10000)>0:
-                return 'You win Level 1!'
-            elif (int(msEquipmentid)-int(sEnemyid)+10000)==0:
-                return 'Your and your enemy all dead!!!'
+    try:
+        data = request.json
+        sEnemyid = data.get('enemyid')
+        msEquipmentid = data.get('equipmentid')
+        
+        if sEnemyid is None:
+            return json.dumps({
+                'code': 9904,
+                'message': 'Enemy ID is required',
+                'data': None
+            })
+        
+        if msEquipmentid is None:
+            return json.dumps({
+                'code': 9905,
+                'message': 'Equipment ID is required',
+                'data': None
+            })
+        
+        if msEquipmentid in ['10001', '10002', '10003']:
+            if sEnemyid in ['20001', '20002', '20003']:
+                battle_result = int(msEquipmentid) - int(sEnemyid) + 10000
+                
+                if battle_result > 0:
+                    result = {'status': 'win', 'level': 1}
+                elif battle_result == 0:
+                    result = {'status': 'draw', 'message': 'Both dead'}
+                else:
+                    result = {'status': 'lose', 'message': 'You died'}
+                    
+                return json.dumps({
+                    'code': 0,
+                    'message': 'Success',
+                    'data': result
+                })
             else:
-                return 'Your dead!'
+                return json.dumps({
+                    'code': 9902,
+                    'message': 'Invalid Enemy ID',
+                    'data': None
+                })
         else:
-            return 'Error 9902: Your kill yourself!!'
-    else:
-        return 'Error 9903: You Broken The Rule!And Kill yourself'
-
+            return json.dumps({
+                'code': 9903,
+                'message': 'Invalid Equipment ID',
+                'data': None
+            })
+    except:
+        return json.dumps({
+            'code': 9906,
+            'message': 'Invalid JSON Input',
+            'data': None
+        })
 
 app = SessionMiddleware(app, session_opts)
 
